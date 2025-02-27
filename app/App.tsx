560dc419fd0ecd45f5e9b4c55065ea96844520ce
tessel-app <159745643+tessel-app@users.noreply.github.com>
2025-02-26 19:47:18 -0800

    Revert to working pravatar URLs for profile images

--- app/App.tsx
+++ app/App.tsx
@@ -4,16 +4,6 @@ import 'react-native-gesture-handler';
 import { AuthProvider } from '../src/context/AuthContext';
 import Navigation from '../src/navigation';
 
-// Import images
-const images = {
-  gracie: require('../assets/images/gracie.jpeg'),
-  aiguo: require('../assets/images/aiguo.png'),
-  moi: require('../assets/images/moi.jpg'),
-  rani: require('../assets/images/rani.png'),
-  shan: require('../assets/images/shan.png'),
-  rolemodel: require('../assets/images/rolemodel.jpg'),
-};
-
 // Mock data can stay for now
 export const mockWorkouts = [
   {
@@ -22,9 +12,9 @@ export const mockWorkouts = [
     time: '2:00 PM',
     location: "Barry's",
     participants: [
-      { id: '1', name: 'Gracie', avatar: images.gracie },
-      { id: '2', name: 'Rani', avatar: images.rani },
-      { id: '3', name: 'Shan', avatar: images.shan },
+      { id: '1', name: 'Gracie', avatar: 'https://i.pravatar.cc/150?img=1' },
+      { id: '2', name: 'Rani', avatar: 'https://i.pravatar.cc/150?img=2' },
+      { id: '3', name: 'Shan', avatar: 'https://i.pravatar.cc/150?img=3' },
     ],
     platforms: ['Strava', 'ClassPass'],
   },
@@ -34,10 +24,10 @@ export const mockWorkouts = [
     time: '8:00 AM',
     location: 'Marina',
     participants: [
-      { id: '4', name: 'Aiguo', avatar: images.aiguo },
-      { id: '5', name: 'Moi', avatar: images.moi },
-      { id: '6', name: 'Rolemodel', avatar: images.rolemodel },
-      { id: '7', name: 'Rani', avatar: images.rani },
+      { id: '4', name: 'Aiguo', avatar: 'https://i.pravatar.cc/150?img=4' },
+      { id: '5', name: 'Moi', avatar: 'https://i.pravatar.cc/150?img=5' },
+      { id: '6', name: 'Rolemodel', avatar: 'https://i.pravatar.cc/150?img=6' },
+      { id: '7', name: 'Rani', avatar: 'https://i.pravatar.cc/150?img=7' },
     ],
     platforms: ['Strava'],
   },
@@ -47,7 +37,7 @@ export const mockWorkouts = [
     time: '8:00 AM',
     location: 'Sonora',
     participants: [
-      { id: '8', name: 'Moi', avatar: images.moi },
+      { id: '8', name: 'Moi', avatar: 'https://i.pravatar.cc/150?img=8' },
     ],
     platforms: ['MindBody'],
   },
